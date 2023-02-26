ab48fe8dc2c008e43692f11e141d7a1a485e848d
ccidbot <ccidbot@angelwork.org>
2023-02-26 03:50:10 +0000

    REL-1.0.3

---
+++ src/tinio.ts
@@ -0,0 +1,120 @@
+import * as WebSocket from "ws"
+
+export type OnTinioConnected = (url: string) => Promise<void>
+export type OnTinioDisconnected = (url: string) => Promise<void>
+export type OnTinioReceived = (url: string, data: any) => Promise<any>
+
+export type WebSocketServerOptions = WebSocket.ServerOptions
+
+export type TinioConfigure = {
+    url: string,        // The url could be reached by the other server
+    options: WebSocketServerOptions,
+    onConnected: OnTinioConnected | undefined,
+    onDisconnected: OnTinioDisconnected | undefined,
+    onReceived: OnTinioReceived | undefined
+}
+
+export class Tinio {
+    /** @internal */ private _wss: WebSocket.WebSocketServer | undefined
+    /** @internal */ private _url: string
+    /** @internal */ private _options: WebSocket.ServerOptions
+    /** @internal */ private _connections: Map<string, WebSocket.WebSocket> = new Map()
+    /** @internal */ private _onTinioConnected: OnTinioConnected | undefined
+    /** @internal */ private _onTinioDisconnected: OnTinioDisconnected | undefined
+    /** @internal */ private _onTinioReceived: OnTinioReceived | undefined
+
+    constructor(cfg: TinioConfigure) {
+        this._url = cfg.url
+        this._options = cfg.options
+        this._onTinioConnected = cfg.onConnected
+        this._onTinioDisconnected = cfg.onDisconnected
+        this._onTinioReceived = cfg.onReceived
+    }
+
+    start(): boolean {
+        this._wss = new WebSocket.WebSocketServer(this._options);
+        this._wss.on('connection', (ws: WebSocket.WebSocket) => {
+            const _tinio = this;
+            ws.onmessage = async (event: WebSocket.MessageEvent) => {
+                try{
+                    const data = JSON.parse(event.data.toString());
+                    if(data.type === "tinio" && data.src_url){
+                        await _tinio._createConnection(data.src_url, ws)
+                    }
+                }catch(e){}
+            }
+        })
+        return true
+    }
+
+    /** @internal */ private async _closeConnection(url: string, ws: WebSocket.WebSocket, removeFromSet: boolean = true){
+        ws.onerror = null
+        ws.onclose = null
+        ws.close()
+        if(removeFromSet) this._connections.delete(url)
+        if(this._onTinioDisconnected) return this._onTinioDisconnected(url)
+    }
+
+    /** @internal */ private async _send(ws: WebSocket.WebSocket, data: any){
+        try{
+            ws.send(JSON.stringify(data))
+        }catch(e){ }
+    }
+
+    /** @internal */ private async _receiveMessage(url: string, ws: WebSocket.WebSocket, data: string){
+        if(this._onTinioReceived){
+            let _data = null
+            try{ _data = JSON.parse(data)}catch(e){}
+            const rep = await this._onTinioReceived(url, _data)
+            if(rep) this._send(ws, rep)
+        }
+    }
+
+    /** @internal */ private async _createConnection(url: string, ws: WebSocket.WebSocket){
+        ws.onerror = async (event: WebSocket.ErrorEvent) => { return this._closeConnection(url, ws) }
+        ws.onclose = async (event: WebSocket.CloseEvent) => { return this._closeConnection(url, ws) }
+        ws.onmessage = async (event: WebSocket.MessageEvent) => {if(this._onTinioReceived) return this._receiveMessage(url, ws, event.data.toString()) }
+        this._connections.set(url, ws)
+        if(this._onTinioConnected) await this._onTinioConnected(url)
+    }
+
+    /** @internal */ private async _getConnectionFromUrl(url: string): Promise<WebSocket.WebSocket|null>{
+        const ews = this._connections.get(url)
+        if(ews) return ews
+        return new Promise((resolve, reject) => {
+            const ws = new WebSocket.WebSocket(url)
+            ws.onopen = async (event: WebSocket.Event) => {
+                ws.send(JSON.stringify({type: "tinio", src_url: this._url}))
+                this._createConnection(url, ws)
+                resolve(ws)
+            }
+            ws.onerror = async (event: WebSocket.ErrorEvent) => { return resolve(null); }
+            ws.onclose = async (event: WebSocket.CloseEvent) => { return resolve(null); }
+        });
+    }
+
+    stop(): void {
+        for (const [url, ws] of this._connections)
+            this._closeConnection(url, ws, true)
+        this._connections.clear()
+        if(this._wss){
+            this._wss.close()
+            this._wss = undefined
+        }
+    }
+
+    /**
+     * Send a message to the given url
+     * @param url the destination url
+     * @param data anything what could be serialized in JSON
+     * @returns true if the data was sent successfully, false otherwise
+     */
+    async send(url: string, data: any): Promise<boolean> {
+        const ws = await this._getConnectionFromUrl(url)
+        if(ws){
+            this._send(ws, data)
+            return true
+        }
+        return false
+    }
+}
---
+++ test/test.ts
@@ -0,0 +1,87 @@
+// Description: Test the tinio module
+
+import process from "process"
+import { Tinio, TinioConfigure } from "../src/tinio.js"
+
+console.log("Begin tinio testing >>>>>>>>>>>")
+
+let tinio1_check = false
+let tinio2_check = false
+
+const tinio1 = new Tinio({
+    url: "ws://localhost:8080",
+    options: {
+        port: 8080
+    },
+    onReceived: (url: string, data: any) => {
+        console.log("tinio1 received message from tinio2")
+        console.log("----- tinio1 received data: ", data)
+        if(data.msg === "Hello") tinio1_check = true
+    }
+} as TinioConfigure)
+
+const tinio2 = new Tinio({
+    url: "ws://localhost:8081",
+    options: {
+        port: 8081
+    },
+    onReceived: (url: string, data: any) => {
+        console.log("tinio2 received message from tinio1")
+        console.log("----- tinio2 received data: ", data)
+        if(data.msg === "Hello") tinio2_check = true
+        return data
+    }
+} as TinioConfigure)
+
+console.log("Starting tinio1")
+tinio1.start()
+
+console.log("Starting tinio2")
+tinio2.start()
+
+console.log("Sending 'Hello' from tinio1 to tinio2")
+tinio1.send("ws://localhost:8081", {msg: "Hello"})
+
+console.log("Waiting for tinio1 to receive 'Hello' from tinio2")
+let check_times = 0
+
+while(!tinio1_check){
+    check_times++
+    if(check_times > 10){
+        console.log("tinio1 did not receive 'Hello' from tinio2")
+        console.log("FAILED")
+        process.exit(1)
+    }
+    await new Promise(resolve => setTimeout(resolve, 1000))
+}
+
+console.log("tinio1 received 'Hello' from tinio2")
+
+tinio1_check = false
+console.log("Sending 'Hello' from tinio2 to tinio1")
+tinio2.send("ws://localhost:8080", {msg: "Hello"})
+
+check_times = 0
+
+while(!tinio1_check){
+    check_times++
+    if(check_times > 10){
+        console.log("tinio1 did not receive 'Hello' from tinio2")
+        console.log("FAILED")
+        process.exit(1)
+    }
+    await new Promise(resolve => setTimeout(resolve, 1000))
+}
+
+
+console.log("tinio1 received 'Hello' from tinio2")
+
+console.log("stopping tinio1")
+tinio1.stop()
+
+console.log("stopping tinio2")
+tinio2.stop()
+
+console.log("PASSED")
+
+console.log("<<<<<<<<<<<<< End tinio testing")
